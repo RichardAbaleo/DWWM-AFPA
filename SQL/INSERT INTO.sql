3446baddbaf7b0b917eca8d07908248de8c11e7a
Richard Abaléo <richard.abaleo.pro@gmail.com>
2021-09-16 16:41:33 +0200

    save #6

---
+++ SQL/INSERT INTO.sql
@@ -0,0 +1,8 @@
+INSERT INTO `orders` (`ord_id`, `ord_order_date`, `ord_payment_date`, `ord_ship_date`, `ord_reception_date`, `ord_status`, `ord_cus_id`) 
+VALUES ('2', '2021-09-01', '2021-09-01', '2021-09-01', '2021-09-01', 'HGFH', '2');
+
+--INSERT INTO `customers` (`cus_id`, `cus_lastname`, `cus_firstname`, `cus_address`, `cus_zipcode`, `cus_city`, `cus_countries_id`, `cus_mail`, `cus_phone`, `cus_password`, `cus_add_date`, `cus_update_date`) VALUES
+--(X , X , X , X , X , X , X , X , X , X , X , X);
+
+
+-- ETC etc... --
---
+++ SQL/requetes-sql.sql
@@ -0,0 +1,63 @@
+-- BASE EXEMPLE --
+
+
+1 - SELECT * FROM employe;
+
+2 - SELECT * FROM dept;
+
+3 - SELECT nom, dateemb, nosup, nodep, salaire FROM employe;
+
+4 - SELECT nom, titre FROM employe;
+
+5 - SELECT DISTINCT nosup, titre FROM employe;
+
+6 - SELECT nom, noemp, nodep FROM employe WHERE titre = "secrétaire";
+
+7 - SELECT nom, nodep FROM employe WHERE nodep > 40;
+
+8 - SELECT nom, prenom FROM employe WHERE nom < prenom;
+
+9 - SELECT nom, salaire, nodep FROM employe WHERE titre = "Représentant" AND nodep = 35 AND salaire > 20000;
+
+10 - SELECT nom, salaire, nodep FROM employe WHERE titre = "Représentant" OR titre = "Président";
+
+11 - SELECT nom, titre, nodep, salaire FROM employe WHERE nodep = 34 AND (titre = "Représentant" OR titre = "Secrétaire");
+
+12 - SELECT nom, titre, nodep, salaire FROM employe WHERE titre = "Représentant" OR (titre = "Secrétaire" AND nodep = 34);
+
+13 - SELECT nom, salaire FROM employe WHERE salaire > 20000 AND salaire < 30000;
+
+15 - SELECT nom FROM employe WHERE nom LIKE "H%";
+
+16 - SELECT nom FROM employe WHERE RIGHT(nom, 1) LIKE "n";
+
+17 - SELECT nom FROM employe WHERE INSTR(nom, "u") = 3;
+
+18 - SELECT salaire, employe.nom FROM employe, dept WHERE nodept = 41 ORDER BY salaire ASC;
+
+19 - SELECT salaire, employe.nom FROM employe, dept WHERE nodept = 41 ORDER BY salaire DESC;
+
+20 - SELECT titre, salaire, nom FROM employe ORDER BY nosup ASC, salaire DESC; 
+
+21 - SELECT tauxcom, salaire, nom FROM employe ORDER BY tauxcom ASC;
+
+22 - SELECT nom, salaire, tauxcom, titre FROM employe WHERE tauxcom IS NULL;
+
+23 - SELECT nom, salaire, tauxcom, titre FROM employe WHERE tauxcom IS NOT NULL;
+
+24 - SELECT nom, salaire, tauxcom, titre FROM employe WHERE tauxcom < 15;
+
+25 - SELECT nom, salaire, tauxcom, titre FROM employe WHERE tauxcom > 15;
+
+26 - SELECT nom, salaire, tauxcom, (tauxcom * salaire) AS "commission" FROM employe WHERE tauxcom IS NOT NULL;
+
+27 - SELECT nom, salaire, tauxcom, (tauxcom * salaire) AS "commission" FROM employe WHERE tauxcom IS NOT NULL ORDER BY "commission" ASC;
+
+28 - SELECT CONCAT(prenom, " ", nom) AS "prenom et nom" FROM employe;
+
+-- BASE HOTEL --
+
+
+1 - SELECT hot_nom AS "Hôtel", hot_ville AS "Ville" FROM hotel;
+
+2 - SELECT cli_nom AS "Nom", cli_ville
