8cdf635f94059dbe44db82a5e892882e630dafeb
RichardAbaleo <richard.abaleo.pro@gmail.com>
2021-09-21 16:30:26 +0200

    21/09/2021

---
+++ VUES/procédures.sql
@@ -0,0 +1,40 @@
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Lst_Suppliers`()
+    NO SQL
+SELECT sup_name FROM suppliers, products, orders_details WHERE suppliers.sup_id = products.pro_sup_id AND products.pro_id = orders_details.ode_pro_id AND ode_id IS NOT NULL GROUP BY sup_name$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Lst_Rush_Orders`(IN `p_libelle` VARCHAR(50))
+    NO SQL
+SELECT ord_id FROM orders WHERE ord_status = p_libelle$$
+DELIMITER ;
+
+
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS CA_Supplier $$
+
+CREATE PROCEDURE CA_Supplier(IN p_id INT, 
+                             IN p_date YEAR, 
+                             OUT p_CA INT
+                             )  
+
+BEGIN
+
+  DECLARE p_existe varchar(50); 
+
+  SET p_existe = (SELECT sup_id FROM suppliers WHERE sup_id = p_id
+                  );
+
+  IF ISNULL(p_existe) 
+  THEN
+     SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "Le client n'existe pas";
+   ELSE      
+      SELECT SUM((ode_unit_price-(ode_unit_price*ode_discount/100))*ode_quantity) INTO p_CA 
+      FROM suppliers, products, orders_details, orders WHERE orders.ord_id = orders_details.ode_ord_id AND suppliers.sup_id = products.pro_sup_id 
+      AND products.pro_id = orders_details.ode_pro_id AND sup_id = p_id AND YEAR(ord_order_date) = p_date;
+   END IF;
+END $$
+
+DELIMITER ;
---
+++ VUES/vues.sql
@@ -0,0 +1,25 @@
+USE hotel;
+CREATE VIEW v_hotel AS SELECT hot_nom, sta_nom FROM hotel, station WHERE hotel.hot_sta_id = station.sta_id;
+SELECT * FROM v_hotel;
+
+CREATE VIEW v_chambre_hotel AS SELECT cha_numero, hot_nom FROM hotel, chambre 
+WHERE hotel.hot_id = chambre.cha_hot_id;
+
+CREATE VIEW v_reservation AS SELECT cha_numero, cli_nom FROM client, chambre, reservation
+WHERE chambre.cha_id = reservation.res_cha_id AND reservation.res_cli_id = client.cli_id;
+
+CREATE VIEW v_cha_hot_sta AS SELECT cha_numero, hot_nom, sta_nom FROM hotel, chambre, station WHERE hotel.hot_id = chambre.cha_hot_id AND hotel.hot_sta_id = station.sta_id;
+SELECT * FROM v_chambre_hotel;
+
+CREATE VIEW v_res_cli_hot AS SELECT cha_numero, cli_nom, hot_nom FROM client, chambre, reservation, hotel 
+WHERE hotel.hot_id = chambre.cha_hot_id AND chambre.cha_id = reservation.res_cha_id AND reservation.res_cli_id = client.cli_id;
+
+
+
+CREATE VIEW v_Details AS SELECT pro_ean AS "Code Produit", ode_quantity AS "QteTot", ode_quantity * ode_unit_price * (1-(0.01 * ode_discount)) AS "PrixTot" 
+FROM products, orders_details WHERE orders_details.ode_pro_id = products.pro_id GROUP BY pro_ean;
+-- (Il n'y a que des code produit NULL dans la base de données hotel fourni, mais celà marche par exemple en utilisant pro_id à la place de pro_ean)
+
+CREATE VIEW v_Ventes_Zoom AS SELECT ode_id, ode_unit_price, ode_discount, ode_quantity FROM orders_details, products WHERE orders_details.ode_pro_id = products.pro_id AND pro_ean = "ZOOM";
+
+-- (idem)
